c1fd49fb7ce8cd5d9ec66081000de3010d3a48a0
Ameya Prabhu <ameya.prabhu@uni-tuebingen.de>
2025-07-10 08:35:14 +0200

    Update publications.js

--- data/publications.js
+++ data/publications.js
@@ -4,8 +4,8 @@ const publicationsData = [
     "id": "chandak2025answermatch",
     "title": "Answer Matching Outperforms Multiple Choice for Language Model Evaluation",
     "authors": [
-      "nikhil_chandak*",
-      "shashwat_goel*",
+      "nikhil_chandak",
+      "shashwat_goel",
       "ameya_prabhu",
       "moritz_hardt",
       "jonas_geiping"
@@ -27,10 +27,11 @@ const publicationsData = [
       "andreas_hochlehnert",
       "hardik_bhatnagar",
       "vishaal_udandarao",
+      "samuel_albanie",
       "ameya_prabhu",
       "matthias_bethge"
     ],
-    "venue": "arXiv",
+    "venue": "COLM",
     "year": "2025",
     "url": "https://arxiv.org/abs/2504.07086",
     "bibtex": "",
@@ -50,7 +51,7 @@ const publicationsData = [
       "matthias_bethge",
       "jonas_geiping"
     ],
-    "venue": "arXiv",
+    "venue": "COLM",
     "year": "2025",
     "url": "https://arxiv.org/abs/2502.19414",
     "bibtex": "",
@@ -89,7 +90,7 @@ const publicationsData = [
       "ameya_prabhu",
       "matthias_bethge"
     ],
-    "venue": "arXiv",
+    "venue": "ICML",
     "year": "2025",
     "url": "https://arxiv.org/abs/2502.04313",
     "bibtex": "",
@@ -172,8 +173,8 @@ const publicationsData = [
       "matthias_bethge",
       "wieland_brendel"
     ],
-    "venue": "arXiv",
-    "year": "2025",
+    "venue": "NeurIPS Workshop",
+    "year": "2024",
     "url": "https://arxiv.org/abs/2502.18326",
     "bibtex": "",
     "links": [
@@ -193,7 +194,7 @@ const publicationsData = [
       "ofir_press",
       "matthias_bethge"
     ],
-    "venue": "arXiv",
+    "venue": "NeurIPS",
     "year": "2024",
     "url": "https://arxiv.org/abs/2407.12861",
     "bibtex": "",
@@ -214,7 +215,7 @@ const publicationsData = [
       "ozan_sener",
       "puneet_dokania"
     ],
-    "venue": "arXiv",
+    "venue": "NeurIPS",
     "year": "2024",
     "url": "https://arxiv.org/abs/2402.08823",
     "bibtex": "",
@@ -237,7 +238,7 @@ const publicationsData = [
       "samuel_albanie",
       "matthias_bethge"
     ],
-    "venue": "arXiv",
+    "venue": "NeurIPS",
     "year": "2024",
     "url": "https://arxiv.org/abs/2404.04125",
     "bibtex": "",
@@ -257,7 +258,7 @@ const publicationsData = [
       "siddharth_bhat",
       "matthias_bethge"
     ],
-    "venue": "arXiv",
+    "venue": "NeurIPS Workshop",
     "year": "2024",
     "url": "https://arxiv.org/abs/2404.06405",
     "bibtex": "",
@@ -278,7 +279,7 @@ const publicationsData = [
       "adel_bibi",
       "samuel_albanie"
     ],
-    "venue": "arXiv",
+    "venue": "NeurIPS",
     "year": "2024",
     "url": "https://arxiv.org/abs/2402.19472",
     "bibtex": "",
@@ -301,7 +302,7 @@ const publicationsData = [
       "ameya_prabhu",
       "philip_torr"
     ],
-    "venue": "arXiv",
+    "venue": "TMLR",
     "year": "2024",
     "url": "https://arxiv.org/abs/2404.09447",
     "bibtex": "",
@@ -341,7 +342,7 @@ const publicationsData = [
       "philip_torr",
       "adel_bibi"
     ],
-    "venue": "arXiv",
+    "venue": "CoLLAs",
     "year": "2023",
     "url": "https://arxiv.org/abs/2311.11293",
     "bibtex": "",
@@ -361,7 +362,7 @@ const publicationsData = [
       "alicia_parrish",
       "ameya_prabhu"
     ],
-    "venue": "arXiv",
+    "venue": "TMLR (Featured)",
     "year": "2023",
     "url": "https://arxiv.org/abs/2306.09479",
     "bibtex": "",
@@ -398,7 +399,7 @@ const publicationsData = [
       "puneet_dokania",
       "vineet_gandhi"
     ],
-    "venue": "arXiv",
+    "venue": "ICLR",
     "year": "2021",
     "url": "https://arxiv.org/abs/2104.00795",
     "bibtex": "",
@@ -416,7 +417,7 @@ const publicationsData = [
       "philip_torr",
       "puneet_dokania"
     ],
-    "venue": "ECCV",
+    "venue": "ECCV (Oral)",
     "year": "2020",
     "url": "https://arxiv.org/abs/2006.02609",
     "bibtex": "",
@@ -448,7 +449,7 @@ const publicationsData = [
     "id": "munagala2020stqnets",
     "title": "STQ-Nets: Unifying Network Binarization and Structured Pruning",
     "authors": ["Sri Aurobindo Munagala", "Ameya Prabhu", "Anoop Namboodiri"],
-    "venue": "BMVC 2020",
+    "venue": "BMVC",
     "year": "2020",
     "url": "https://arxiv.org/abs/2010.06528",
     "bibtex": "@inproceedings{munagala2020stq,\n  title={STQ-Nets: Unifying Network Binarization and Structured Pruning},\n  author={Munagala, Sri Aurobindo and Prabhu, Ameya and Namboodiri, Anoop},\n  booktitle={British Machine Vision Conference},\n  year={2020}\n}",
@@ -477,7 +478,7 @@ const publicationsData = [
     "id": "prabhu2018adversarybestteacher",
     "title": "Adversary Is the Best Teacher: Towards Extremely Compact Neural Networks",
     "authors": ["Ameya Prabhu", "Harish Krishna", "Soham Saha"],
-    "venue": "AAAI 2018",
+    "venue": "AAAI (Student Abstract)",
     "year": "2018",
     "url": "https://doi.org/10.1609/aaai.v32i1.12345",  /* placeholder DOI */
     "bibtex": "@inproceedings{prabhu2018adversary,\n  title={Adversary Is the Best Teacher: Towards Extremely Compact Neural Networks},\n  author={Prabhu, Ameya and Krishna, Harish and Saha, Soham},\n  booktitle={AAAI Conference on Artificial Intelligence},\n  year={2018}\n}",
@@ -492,7 +493,7 @@ const publicationsData = [
     "id": "prabhu2017expander",
     "title": "Deep Expander Networks: Efficient Deep Networks from Graph Theory",
     "authors": ["Ameya Prabhu", "Girish Varma", "Anoop Namboodiri"],
-    "venue": "arXiv",
+    "venue": "ECCV (Oral)",
     "year": "2017",
     "url": "https://arxiv.org/abs/1711.08757",
     "bibtex": "@article{prabhu2017expander,\n  title={Deep Expander Networks: Efficient Deep Networks from Graph Theory},\n  author={Prabhu, Ameya and Varma, Girish and Namboodiri, Anoop},\n  journal={arXiv preprint arXiv:1711.08757},\n  year={2017}\n}",
@@ -507,7 +508,7 @@ const publicationsData = [
     "id": "joshi2016sentiment",
     "title": "Towards Sub-Word-Level Compositions for Sentiment Analysis of Hindi-English Code-Mixed Text",
     "authors": ["Aditya Joshi", "Ameya Prabhu", "Manish Shrivastava", "Vasudeva Varma"],
-    "venue": "COLING 2016",
+    "venue": "COLING",
     "year": "2016",
     "url": "https://aclanthology.org/C16-1234",
     "bibtex": "@inproceedings{joshi2016sentiment,\n  title={Towards Sub-Word Level Compositions for Sentiment Analysis of Hindi-English Code-Mixed Text},\n  author={Joshi, Aditya and Prabhu, Ameya and Shrivastava, Manish and Varma, Vasudeva},\n  booktitle={International Conference on Computational Linguistics},\n  year={2016}\n}",
@@ -520,7 +521,7 @@ const publicationsData = [
     "id": "athavale2016hineri",
     "title": "Towards Deep Learning in Hindi NER: Tackling Labelled-Data Sparsity",
     "authors": ["Vinayak Athavale", "Shreenivas Bharadwaj", "Ameya Prabhu", "et al."],
-    "venue": "ICON 2016",
+    "venue": "ICON",
     "year": "2016",
     "url": "https://aclanthology.org/W16-3210",
     "bibtex": "@inproceedings{athavale2016hineri,\n  title={Towards Deep Learning in Hindi NER: An Approach to Tackle Labelled Data Sparsity},\n  author={Athavale, Vinayak and Bharadwaj, Shreenivas and Prabhu, Ameya and others},\n  booktitle={International Conference on Natural Language Processing (ICON)},\n  year={2016}\n}",
